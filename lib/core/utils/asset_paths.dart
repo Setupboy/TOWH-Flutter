c420d4effdc987a8d109241a3f91a7dc9ae7668f
elham96dev <elham96dev@gmail.com>
2026-03-31 00:12:01 +0400

    onfigure Android release signing with upload keystore

--- lib/core/utils/asset_paths.dart
+++ lib/core/utils/asset_paths.dart
@@ -1,6 +1,6 @@
 // Onboarding
-const kOnboardingStep1Img = 'assets/images/onboardings/first.png';
-const kOnboardingStep2Img = 'assets/images/onboardings/second.png';
-const kOnboardingStep3Img = 'assets/images/onboardings/third.png';
+const kOnboardingStep1Img = 'assets/images/onboardings/first.webp';
+const kOnboardingStep2Img = 'assets/images/onboardings/second.webp';
+const kOnboardingStep3Img = 'assets/images/onboardings/third.webp';
 
 // Login
